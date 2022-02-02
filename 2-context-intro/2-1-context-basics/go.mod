435c935dffefa1cea22a2365408bd7739a0604c1
Marco Rivera <marco.rivera.neri@gmail.com>
2022-02-02 11:21:31 -0600

    Added context basics section

---
+++ 2-context-intro/2-1-context-basics/go.mod
@@ -0,0 +1,3 @@
+module contextBasics
+
+go 1.17
--- 2-context-intro/2-1-context-basics/main.go
+++ 2-context-intro/2-1-context-basics/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "context"
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
 
 func main() {
+	withCancel()
+}
+
+func withCancel() {
 	ctx := context.Background()
+
+	ctx, cancel := context.WithCancel(ctx)
+
+	go func() {
+		s := bufio.NewScanner(os.Stdin)
+		s.Scan()
+		cancel()
+	}()
+	sleepAndTalk(ctx, 5*time.Second, "hello with cancel")
+}
+
+func withDuration() {
+	ctx := context.Background()
+
+	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
+	defer cancel()
+
+	sleepAndTalk(ctx, 5*time.Second, "hello with timeout")
+}
+
+func sleepAndTalk(ctx context.Context, d time.Duration, msg string) {
+	select {
+	case <-time.After(d):
+		fmt.Println(msg)
+	case <-ctx.Done():
+		log.Println(ctx.Err())
+	}
 }
